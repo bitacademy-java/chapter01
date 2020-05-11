b5d039ce6220f5eddd79f3dca6200e5b70da8a5b
bit <bit@DESKTOP-2GQ31VL>
2020-05-11 20:55:45 +0900

    연산자 & if 구문 연습

---
+++ src/chapte01/Logical.java
@@ -0,0 +1,29 @@
+package chapte01;
+
+public class Logical {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		
+		// f || t => t
+		// t || f => t
+		// t || t => t
+		// f || f => f
+		System.out.println( a == b ||  a > b);
+
+		// f && t => f
+		// t && f => f
+		// t && t => t
+		// f && f => f
+		System.out.println( a == b &&  a > b);
+		
+		System.out.println( a != b || a > b);
+		System.out.println( a != b && a > b);
+		
+		// not
+		System.out.println( !(a > b) );   // a <= b
+		
+	}
+
+}
---
+++ src/chapte01/Relation.java
@@ -0,0 +1,25 @@
+package chapte01;
+
+public class Relation {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		
+		System.out.println(a == b);
+		System.out.println(a != b);
+
+		System.out.println(a > b);
+		System.out.println(a < b);
+		
+		System.out.println(a >= b);
+		System.out.println(a <= b);
+
+		System.out.println(a >= b - 10);
+		System.out.println(a - 10 <= b);
+
+		boolean bool = (a == b);
+		System.out.println(bool);
+	}
+
+}
